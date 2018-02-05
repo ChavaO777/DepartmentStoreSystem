34ca85fbc51e1ccd2df7404327d6bfad28a2145a
ChavaO777 <salvador@orozco.in>
2018-02-05 00:10:45 -0600

    Refined the getProductsToBuy() function to get only the products with an actual number in the input of the amount

--- startbootstrap-freelancer-gh-pages/js/sale.js
+++ startbootstrap-freelancer-gh-pages/js/sale.js
@@ -4,8 +4,12 @@ function getProductsToBuy(){
     var productsToBuy = [];
 
     $('.product-amount').each(function () {
-        // termTableInputIDs.push($(this).attr('id'));
-        productsToBuy.push({id : $(this).attr('id'), amount : $(this).val()})
+
+        var product_id = $(this).attr('id');
+        var product_amount = $(this).val();
+
+        if(!isNaN(product_amount))
+            productsToBuy.push({id : product_id, amount : product_amount});
     });
 
     console.log(JSON.stringify(productsToBuy));
