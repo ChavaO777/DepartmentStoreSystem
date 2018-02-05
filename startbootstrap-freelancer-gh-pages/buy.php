e54f7586f3ab1abc40a18503e32c4d91fb15be94
Aranzza Abascal <arabascalf@gmail.com>
2018-02-05 13:37:15 -0600

    Changes on buy.php

---
+++ startbootstrap-freelancer-gh-pages/buy.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="Liverpool database">
+        <meta name="author" content="Salvaor & Aranzza">
+
+        <title>Liverpool</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom fonts for this template -->
+        <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+        <!-- Plugin CSS -->
+        <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
+
+        <!-- Custom styles for this template -->
+        <link href="css/freelancer.min.css" rel="stylesheet">
+
+    </head>
+
+     <body id="page-top">
+    
+        <nav class="navbar navbar-expand-lg bg-secondary fixed-top text-uppercase" id="mainNav">
+
+            <!-- División de la primera sección -->
+            <div class="container">
+
+            <!-- Título "Liverpool" en la parte superior izquierda -->
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">Liverpool</a>
+        </nav>
+    </body>
+</html>
