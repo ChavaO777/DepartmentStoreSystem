6b59412b322c7df78e0be5ed9bbcd97615d1c380
ChavaO777 <salvador@orozco.in>
2018-02-05 13:37:29 -0600

    Added an access key to the foreach -> Parsing the url parameters and placing them in an array

--- startbootstrap-freelancer-gh-pages/buy.php
+++ startbootstrap-freelancer-gh-pages/buy.php
@@ -4,11 +4,12 @@
     </head>
     <body>
         <?php 
+
             parse_str($_SERVER["QUERY_STRING"], $query_array);
             // echo $_SERVER['QUERY_STRING']; 
 
-            foreach($query_array as &$value) {
-                echo $value;
+            foreach($query_array as $key => $value) {
+                echo 'product_id = ' . $key . '; amount = ' . $value;
             }
         ?>
     </body>
