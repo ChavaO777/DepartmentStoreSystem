e9cae6be122d3858d849279e425a894f65ad78c1
ChavaO777 <salvador@orozco.in>
2018-02-05 00:16:31 -0600

    ADDED: a condition to check the length of the string -> Refined the getProductsToBuy() function to get only the products with an actual number in the input of the amount

--- startbootstrap-freelancer-gh-pages/js/sale.js
+++ startbootstrap-freelancer-gh-pages/js/sale.js
@@ -8,7 +8,7 @@ function getProductsToBuy(){
         var product_id = $(this).attr('id');
         var product_amount = $(this).val();
 
-        if(!isNaN(product_amount))
+        if(product_amount.length > 0 && !isNaN(product_amount))
             productsToBuy.push({id : product_id, amount : product_amount});
     });
 
