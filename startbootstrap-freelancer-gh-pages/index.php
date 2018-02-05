b794a027da9fa861d54ff873b728c01364fc8dc6
Aranzza Abascal <arabascalf@gmail.com>
2018-02-04 22:07:54 -0600

    New button to confirm

--- startbootstrap-freelancer-gh-pages/index.php
+++ startbootstrap-freelancer-gh-pages/index.php
@@ -109,6 +109,7 @@
             </tbody>
           </table>
         </div>
+        <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#comprar">Comprar</a>
     </section> 
 
 
