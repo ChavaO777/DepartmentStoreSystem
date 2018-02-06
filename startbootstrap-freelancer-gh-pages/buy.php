bfddb2c5f1a2255cf47ebbb66f354edb2b143b1a
Aranzza Abascal <arabascalf@gmail.com>
2018-02-05 22:09:10 -0600

    Insert into sale query

--- startbootstrap-freelancer-gh-pages/buy.php
+++ startbootstrap-freelancer-gh-pages/buy.php
@@ -44,9 +44,9 @@
             $q = $pdo->prepare($sql);
             $q->execute(array($name, $lastname));
 
-            $sql = "INSERT INTO sale (id,customer, date_time) values(null,?,?)";         
-            $q = $pdo->prepare($sql);
-            $q->execute(array($name, NOW()));
+            $sql1 = "INSERT INTO sale (id,customer, date_time) values(null,?,?)";         
+            $q1 = $pdo->prepare($sql1);
+            $q1->execute(array($name, NOW()));
 
             Database::disconnect();
             header("Location: index.php");
