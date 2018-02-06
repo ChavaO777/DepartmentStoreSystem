d3dfc4c1e34b263ddae048bbe88f32838f7a37a1
Aranzza Abascal <arabascalf@gmail.com>
2018-02-05 21:40:31 -0600

    Modified text of table

--- startbootstrap-freelancer-gh-pages/buy.php
+++ startbootstrap-freelancer-gh-pages/buy.php
@@ -157,7 +157,7 @@
                         
                         echo '<tr>';
                         echo '<td></td>';
-                        echo '<td text-uppercase text-secondary>Monto total de la compra</td>';
+                        echo '<td class="text-uppercase text-secondary">Monto total de la compra</td>';
                         echo '<td>' . $sale_total_amount . '</td>';
                         echo '</tr>';
                         Database::disconnect();
