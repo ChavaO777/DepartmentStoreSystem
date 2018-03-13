86f1248cdb32c36bbdbcf0fb4d714fe3230c08ec
Aranzza Abascal <arabascalf@gmail.com>
2018-03-12 20:22:48 -0600

    Added button to add dentist

--- HW/Partial2/FinalPartialHomework/bootstrap.php
+++ HW/Partial2/FinalPartialHomework/bootstrap.php
@@ -97,79 +97,84 @@
         </div>
       </div>
       <div class="row">
-          <div class="col-sm-4">
-            <div class="team-member">
-              <img class="mx-auto rounded-circle" src="img/team/1.jpg" alt="">
-              <h4 style="color: #ffc107">Diana Garland</h4>
-              <p class="text-muted">General dentist</p>
-            </div>
-          </div>
-          <div class="col-sm-4">
-            <div class="team-member">
-              <img class="mx-auto rounded-circle" src="img/team/2.jpg" alt="">
-              <h4 style="color: #ffc107">Larry Parker</h4>
-              <p class="text-muted">Endodontist</p>
-            </div>
-          </div>
-          <div class="col-sm-4">
-            <div class="team-member">
-              <img class="mx-auto rounded-circle" src="img/team/3.jpg" alt="">
-              <h4 style="color: #ffc107">Tyler Pertersen</h4>
-              <p class="text-muted">Prosthodontist</p>
-            </div>
+        <div class="col-sm-4">
+          <div class="team-member">
+            <img class="mx-auto rounded-circle" src="img/team/1.jpg" alt="">
+            <h4 style="color: #ffc107">Diana Garland</h4>
+            <p class="text-muted">General dentist</p>
           </div>
         </div>
-        <div class="col-lg-12">
-                <?php
-                if( ! empty( $dentists ) ) {
-                ?>
-                <table class="table table-striped">
-                    <tr>
-                        <!-- <th>Id</th> -->
-                        <th>First name</th>
-                        <th>Last name</th>
-                        <th>Cellphone</th>
-                        <th>Email</th>
-                        <th>Birthdate</th>
-                        <th>Start date</th>
-                        <th>Status</th>
-                        <!-- <th>Edit</th>
-                        <th>Delete</th> -->
-                    </tr>
-                    <?php foreach( $dentists as $dentist )
-                    {
-                    ?>
-                        <tr>
-                            <!-- <td><?php //echo $dentist->id ?></td> -->
-                            <td><?php echo $dentist->first_name ?></td>
-                            <td><?php echo $dentist->last_name ?></td>
-                            <td><?php echo $dentist->cellphone ?></td>
-                            <td><?php echo $dentist->email ?></td>
-                            <td><?php echo $dentist->birthdate ?></td>
-                            <td><?php echo $dentist->start_date ?></td>
-                            <td><?php echo $dentist->status==1 ? "<font color='green'>Active</font>" : "<font color='red'>Deactivated</font>" ?></td>
-                            
-                            <!-- <td>
-                                <a class="btn btn-info" href="<?php echo Dentist::baseurl() ?>app/editDentist.php?dentist=<?php echo $dentist->id ?>">Edit</a> 
-                            </td>
-                            <td>
-                                <a class="btn btn-info" href="<?php echo Dentist::baseurl() ?>app/deleteDentist.php?dentist=<?php echo $dentist->id ?>">Delete</a>
-                            </td> -->
-                        </tr>
-                    <?php
-                    }
-                    ?>
-                </table>
-                <?php
-                }
-                else
-                {
-                ?>
-                <div class="alert alert-danger" style="margin-top: 100px">There are 0 registered dentists</div>
-                <?php
-                }
-                ?>
-            </div>
+        <div class="col-sm-4">
+          <div class="team-member">
+            <img class="mx-auto rounded-circle" src="img/team/2.jpg" alt="">
+            <h4 style="color: #ffc107">Larry Parker</h4>
+            <p class="text-muted">Endodontist</p>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="team-member">
+            <img class="mx-auto rounded-circle" src="img/team/3.jpg" alt="">
+            <h4 style="color: #ffc107">Tyler Pertersen</h4>
+            <p class="text-muted">Prosthodontist</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-12">
+        <?php
+        if( ! empty( $dentists ) ) {
+        ?>
+        <table class="table table-striped">
+          <tr>
+            <!-- <th>Id</th> -->
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Cellphone</th>
+            <th>Email</th>
+            <th>Birthdate</th>
+            <th>Start date</th>
+            <th>Status</th>
+            <!-- <th>Edit</th>
+            <th>Delete</th> -->
+          </tr>
+          <?php foreach( $dentists as $dentist )
+          {
+          ?>
+          <tr>
+          <!-- <td><?php //echo $dentist->id ?></td> -->
+            <td><?php echo $dentist->first_name ?></td>
+            <td><?php echo $dentist->last_name ?></td>
+            <td><?php echo $dentist->cellphone ?></td>
+            <td><?php echo $dentist->email ?></td>
+            <td><?php echo $dentist->birthdate ?></td>
+            <td><?php echo $dentist->start_date ?></td>
+            <td><?php echo $dentist->status==1 ? "<font color='green'>Active</font>" : "<font color='red'>Deactivated</font>" ?></td>
+                      
+            <!-- <td>
+            <a class="btn btn-info" href="<?php echo Dentist::baseurl() ?>app/  editDentist.php?dentist=<?php echo $dentist->id ?>">Edit</a> 
+            </td>
+            <td>
+            <a class="btn btn-info" href="<?php echo Dentist::baseurl() ?>app/deleteDentist.php?dentist=<?php echo $dentist->id ?>">Delete</a>
+            </td> -->
+          </tr>
+          <?php
+          }
+          ?>
+        </table>
+        <?php
+        }
+        else
+        {
+        ?>
+        <div class="alert alert-danger" style="margin-top: 100px">There are 0 registered dentists</div>
+          <?php
+          }
+          ?>
+      </div>
+
+      <div class="intro-text">
+        <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Add dentist</a>
+      </div>
     </section>
 
     <!-- patients -->
